9f7accd7a370a891ffc8a4bc1e73d170626aa6b0
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-04-05 20:51:48 +0200

    added solutions for class 0

---
+++ G2/Class 0/CSharpAdvanced-G2-L0-EX2/Program.cs
@@ -0,0 +1,72 @@
+﻿List<DateTime> nonWorkingDays = new List<DateTime>()
+{
+    new DateTime(2001, 1, 1),
+    new DateTime(2001, 1, 7),
+    new DateTime(2001, 4, 20),
+    new DateTime(2001, 5, 1),
+    new DateTime(2001, 5, 25),
+    new DateTime(2001, 8, 3),
+    new DateTime(2001, 9, 8),
+    new DateTime(2001, 10, 12),
+    new DateTime(2001, 10, 23),
+    new DateTime(2001, 12, 8)
+};
+
+do
+{
+
+    Console.WriteLine("Enter day for date");
+    string dayInput = Console.ReadLine();
+
+    bool isDayParsed = int.TryParse(dayInput, out int dayParsed);
+
+    if (!isDayParsed)
+    {
+        Console.WriteLine("Invalid input for day day");
+        return;
+    }
+
+    Console.WriteLine("Enter month for date");
+    string monthInput = Console.ReadLine();
+
+    bool isMonthParsed = int.TryParse(monthInput, out int monthParsed);
+
+    if (!isMonthParsed)
+    {
+        Console.WriteLine("Invalid input for month");
+        return;
+    }
+
+    Console.WriteLine("Enter year for date");
+    string yearInput = Console.ReadLine();
+
+    bool isYearParsed = int.TryParse(yearInput, out int yearParsed);
+
+    if (!isYearParsed)
+    {
+        Console.WriteLine("Invalid input for year");
+        return;
+    }
+
+    DateTime date = new DateTime(yearParsed, monthParsed, dayParsed);
+
+    foreach (DateTime nonWorkingDay in nonWorkingDays)
+    {
+        if (date.Day == nonWorkingDay.Day && date.Month == nonWorkingDay.Month)
+        {
+            Console.WriteLine($"{date} is a non working day");
+        }
+    }
+
+    if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+    {
+        Console.WriteLine($"{date} is on weekend {date.DayOfWeek}");
+    }
+    else
+    {
+        Console.WriteLine($"{date} is a working day");
+    }
+
+    Console.WriteLine("Check another date? y/n");
+
+} while (Console.ReadLine().ToLower() == "y");
---
+++ G2/Class 0/CSharpAdvanced-G2-L0-EX3/Program.cs
@@ -0,0 +1,109 @@
+﻿
+string option;
+double computerWins = 0;
+double playerWins = 0;
+double totalGames = 0;
+do
+{
+    PrintMenu();
+    option = Console.ReadLine();
+
+    switch (option)
+    {
+        case "1":
+            Console.WriteLine("You have chosen to play");
+            int outcome = PlayGame();
+
+            if (outcome == 1)
+            {
+                playerWins += 1;
+            }
+            else if (outcome == 2)
+            {
+                computerWins += 1;
+            }
+
+            totalGames += 1;
+            break;
+        case "2":
+            Console.WriteLine("You have chosen to see the stats");
+            ShowStats(computerWins, playerWins, totalGames);
+            break;
+        case "3":
+            Console.WriteLine("You chose to exit");
+            break;
+    }
+}
+while (option != "3");
+
+void ShowStats(double computerWins, double playerWins, double totalGames)
+{
+    Console.WriteLine($"Computer Wins: {computerWins} Player Wins: {playerWins}");
+    Console.WriteLine($"The player win percentage is {string.Format("{0:P2}", playerWins/(totalGames * 100))}");
+}
+
+int PlayGame()
+{
+    List<string> options = new List<string>() { "rock", "paper", "scissors" };
+    Dictionary<Tuple<string, string>, int> outcome = new Dictionary<Tuple<string, string>, int>()
+    {
+        { Tuple.Create("rock", "rock"),  0 },
+        { Tuple.Create("rock", "scissors"),  1 },
+        { Tuple.Create("scissors", "rock"),  2 },
+        { Tuple.Create("scissors", "scissors"),  0 },
+        { Tuple.Create("scissors", "paper"),  1 },
+        { Tuple.Create("paper", "scissors"),  2 },
+        { Tuple.Create("paper", "paper"),  0 },
+        { Tuple.Create("paper", "rock"),  1 },
+        { Tuple.Create("rock", "paper"),  2 },
+       
+    };
+
+    // rock beats scissros 1
+    // scissors cuts paper 2
+    // paper beats rock
+    // equals 
+    // equals -> 0
+    // user wins -> 1
+    // computer wins -> 2
+
+    Console.WriteLine("User, enter rock,paper or scissors");
+    string userSelection = Console.ReadLine().ToLower();
+
+    if (!options.Contains(userSelection))
+    {
+        Console.WriteLine("Invalid input, exiting play");
+        return -1;
+    }
+
+    Console.WriteLine("Computer is choosing move");
+
+    Random random = new Random();
+    int computerSelectionIndex = random.Next(0, 3); // Next() generates a number between 0 and 3 without including 3 in the range
+    string computerSelection = options[computerSelectionIndex];
+
+    Console.WriteLine($"User choice is {userSelection} and computer choice is {computerSelection}");
+
+    int outcomeValue = outcome[Tuple.Create(userSelection, computerSelection)];
+
+    if (outcomeValue == 0)
+    {
+        Console.WriteLine("DRAW!");
+    }
+    else if (outcomeValue == 1)
+    {
+        Console.WriteLine("User wins");
+ 
+    }
+    else
+    {
+        Console.WriteLine("Computer Wins");
+    }
+
+    return outcomeValue;
+}
+
+static void PrintMenu()
+{
+    Console.WriteLine("Pick Option:\n1) Play \n2) Stats \n3) Exit");
+}
