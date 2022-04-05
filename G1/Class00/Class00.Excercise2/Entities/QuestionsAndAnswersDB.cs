94b9765186e41ab6656c212f8556a344179a3b73
Hristijan Taseski <hristijan.taseski@gmail.com>
2022-04-05 23:21:05 +0200

    quizzApp
    exercise 2

---
+++ G1/Class00/Class00.Excercise2/Entities/QuestionsAndAnswersDB.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class00.Excercise2.Entities
+{
+    public static class QuestionsAndAnswersDB
+    {
+        public static List<Questions> Questions = new List<Questions>()
+        {
+            new Questions()
+            {
+                Question = "What is the capital of Tasmania?",
+                 Answers  = new List<string>
+                {
+                    "a: Dodoma",
+                    "b: Hobart",
+                    "c: Launceston",
+                    "d: Wellington"
+                },
+                CorrectAnswer = 2
+            },
+            new Questions()
+            {
+                Question = "What is the tallest building in the Republic of the Congo?",
+                Answers  = new List<string>
+                {
+                    "a: Kinshasa Democratic Republic of the Congo Temple",
+                    "b: Palais de la Nation",
+                    "c: Kongo Trade Centre",
+                    "d: Nabemba Tower"
+                },
+                CorrectAnswer = 4
+            },
+            new Questions()
+            {
+                Question = "Which of these is not one of Pluto's moons?",
+                Answers  = new List<string>
+                {
+                    "a: Styx",
+                    "b: Hydra",
+                    "c: Nix",
+                    "d: Lugia"
+                },
+                CorrectAnswer = 3
+            },
+            new Questions()
+            {
+                Question = "What is the smallest lake in the world?",
+                Answers  = new List<string>
+                {
+                    "a: Onega Lake",
+                    "b: Benxi Lake",
+                    "c: Kivu Lake",
+                    "d: Wakatipu Lake"
+                },
+                CorrectAnswer = 2
+            },
+            new Questions()
+            {
+                Question = "What country has the largest population of alpacas?",
+                Answers  = new List<string>
+                {
+                    "a: Chad",
+                    "b: Peru",
+                    "c: Australia",
+                    "d: Niger"
+                },
+                CorrectAnswer = 2
+            },
+        };
+    }
+}
---
+++ G1/Class00/Class00.Excercise2/Program.cs
@@ -0,0 +1,98 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using Class00.Excercise2.Entities;
+using Class00.Excercise2.Services;
+
+
+Services services = new Services();
+
+
+Console.WriteLine("Hello and welcome to our quiz app !!!!");
+
+while (true)
+{
+    try
+    {
+
+        Console.WriteLine("Please enter username");
+        string username = Console.ReadLine();
+        if (string.IsNullOrEmpty(username))
+        {
+            throw new Exception("Please enter username, it's mandatory field");
+        }
+
+
+        Student student = services.GetStudentByUserName(username);
+
+        if (student != null)
+        {
+
+            if (student == null)
+            {
+                throw new Exception("The user does not exist");
+            }
+            bool isPasswordCorrect = services.PasswordCheck(student.Password);
+            if (!isPasswordCorrect)
+            {
+                throw new Exception("Password did not match 3 times. Try again after 30 minutes");
+            }
+            if (student.isQuizzFinished)
+            {
+                throw new Exception("You already did the quiz");
+            }
+
+
+            services.StartQuiz(student);
+
+        }
+        else
+        {
+            Teacher teacher = services.GetTeacherByUserName(username);
+
+            if (teacher == null)
+            {
+                throw new Exception("The user does not exist");
+            }
+            bool isPasswordCorrect = services.PasswordCheck(teacher.Password);
+            if (!isPasswordCorrect)
+            {
+                Console.WriteLine("You have entered incorrect password three times! The application will close!");
+                //Console.ReadLine();
+                Environment.Exit(0);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("The students that take the quiz are: ");
+            foreach (Student students in services.GetStudentsThatStartTheQuiz())
+            {
+                Console.WriteLine($"{students.FirstName} {students.LastName} with {students.CorrectAnswers} correct answers");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("The students that dont take the quiz are: ");
+            foreach (Student students in services.GetStudentsThatNotStartTheQuiz())
+            {
+                Console.WriteLine($"{students.FirstName} {students.LastName}");
+            }
+
+            Console.ResetColor();
+
+
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("An error occured");
+        Console.WriteLine(ex.Message);
+    }
+}
+
+
+
+
+
+
+
+
+ 
