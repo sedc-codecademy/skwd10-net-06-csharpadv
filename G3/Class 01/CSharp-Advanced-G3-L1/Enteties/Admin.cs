5229367f1ccb2a34c096218c23a7a9379aea6d69
Code <code@sedc.mk>
2022-04-06 18:35:47 +0200

    Added class 01 first class code

---
+++ G3/Class 01/CSharp-Advanced-G3-L1/Enteties/Admin.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enteties
+{
+    public class Admin : Person
+    {
+        public string Department { get; set; }
+
+        public Admin(int id, string name, string lastName, string department) : base(id, name, lastName)
+        {
+            Department = department;
+        }
+
+        public override string GetInfo()
+        {
+            return $"{Name} {LastName} is admin to department {Department}";
+        }
+    }
+}
