97fad1c090926a8fd8a86fcaac32118f7b50c534
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-04-07 18:26:25 +0200

    added correct code

--- G2/Class 1/CSharpAdvanced-G2-L1-AbstractClasses-Interfaces/CSharpAdvanced-G2-L1-AbstractClasses-Interfaces/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using CSharpAdvanced_G2_L1_AbstractClasses_Interfaces.Entities;
-
-Console.WriteLine("Hello, World!");
-
-Bird bird = new Bird();
--- G2/Class 1/CSharpAdvanced-G2-L1-AbstractClasses/CSharpAdvanced-G2-L1-AbstractClasses/Entities/Bird.cs
+++ G2/Class 1/CSharpAdvanced-G2-L1-AbstractClasses/CSharpAdvanced-G2-L1-AbstractClasses/Entities/Bird.cs
@@ -1,14 +1,14 @@
-﻿namespace CSharpAdvanced_G2_L1_AbstractClasses_Interfaces.Entities
+﻿namespace CSharpAdvanced_G2_L1_AbstractClasses.Entities
 {
     public abstract class Bird
     {
         public string Color { get; set; }
 
-        public int FlightSpeed { get; set; } 
+        public int FlightSpeed { get; set; }
 
         public int Height { get; set; }
 
-        public Bird(string color, int flightSpeed, int height)
+        protected Bird(string color, int flightSpeed, int height)
         {
             Color = color;
             FlightSpeed = flightSpeed;
