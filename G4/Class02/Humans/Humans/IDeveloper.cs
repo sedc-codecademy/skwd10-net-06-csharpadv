ba8c4402d951cd8aa88fd2631e241d365cf23aac
Vladimir Simonovski <vladimir.simonovski@gmail.com>
2022-04-08 11:07:35 +0200

    Humans example

---
+++ G4/Class02/Humans/Humans/IDeveloper.cs
@@ -0,0 +1,11 @@
+﻿namespace Humans
+{
+    /// <summary>
+    /// Interface for modelling humans that can code
+    /// </summary>
+    internal interface IDeveloper
+    {
+        // Anyone who implements this interface must have a Code() method that does not return anything
+        void Code();
+    }
+}
