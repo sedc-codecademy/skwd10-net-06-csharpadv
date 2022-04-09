8a95dc47246e09c5746c705a674dc00cf7b5cf62
Code <code@sedc.mk>
2022-04-09 12:17:37 +0200

    class 2

---
+++ G3/Class 02/CSharp-Advanced-G3-L2/CSharp-Advanced-G3-L2-Poly/Enteties/Dog.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Advanced_G3_L2_Poly.Enteties
+{
+    public class Dog : Pet
+    {
+        public string Breed { get; set; }
+
+        public Dog() : base("Sharko")
+        {
+
+        }
+
+        public Dog(string name) : base(name)
+        {
+
+        }
+
+        public Dog(int number) : base("1234")
+        {
+            Console.WriteLine(number);
+        }
+
+        public Dog(string name, string breed) : base(name)
+        {
+            Breed = breed;
+        }
+
+        public override void Eat()
+        {
+            Console.WriteLine($"The dog {Breed} is eating");
+        }
+    }
+}
---
+++ G3/Class 02/CSharp-Advanced-G3-L2/CSharp-Advanced-G3-L2-Poly/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharp_Advanced_G3_L2_Poly.Enteties;
+
+Console.WriteLine("Hello, World!");
+
+List<Pet> pets = new List<Pet>()
+{
+    new Cat("meow", 2),
+    new Dog("bark", "German Sheppard"),
+    new Dog("sparky", "Doga")
+};
+
+foreach(Pet pet in pets)
+{
+    pet.Eat();
+}
+
+Dog dog1 = new Dog();
+dog1.Eat();
+Dog dog2 = new Dog(123);
+Dog dog3 = new Dog("Bak");
+Dog dog4 = new Dog("DogName", "Koker");
+
+Console.ReadLine();
+
+MethodPoly methodPoly = new MethodPoly();
+
+methodPoly.AddNumbers(3, 7);
+methodPoly.AddNumbers(5.3, 4.6);
+methodPoly.AddNumbers(1, (double)5);
+methodPoly.AddNumbers(6, 4, 5);
+
