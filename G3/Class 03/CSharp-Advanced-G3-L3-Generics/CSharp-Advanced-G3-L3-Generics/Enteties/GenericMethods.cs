11b6bcda00ae900b3d558acc0d6026aa4cb8c2ca
Code <code@sedc.mk>
2022-04-11 19:46:32 +0200

    Generics code

---
+++ G3/Class 03/CSharp-Advanced-G3-L3-Generics/CSharp-Advanced-G3-L3-Generics/Enteties/GenericMethods.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Advanced_G3_L3_Generics.Enteties
+{
+    public class GenericMethods
+    {
+        public string MyProperty { get; set; }
+
+        public void PrintObject<TFirst, TSecond>(TFirst objetToPrint, TSecond secondObject)
+        {
+            Console.WriteLine(objetToPrint);
+            Console.WriteLine(typeof(TFirst));
+            Console.WriteLine(typeof(TSecond));
+            Console.WriteLine(secondObject);
+        }
+    }
+}
