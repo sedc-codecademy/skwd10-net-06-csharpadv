98dd3b405a67d70a04e5aad7c58c8d81143745dd
Code <code@sedc.mk>
2022-04-11 20:57:02 +0200

    Added extension methods code

---
+++ G3/Class 03/CSharp-Advanced-G3-L3-Generics/CSharp-Advanced-G3-L3-Extensions/Extensions/StringExtensions.cs
@@ -0,0 +1,21 @@
+﻿
+namespace System
+{
+    public static class StringExtensions
+    {
+        public static string DeleteLastCharacter(this string text)
+        {
+            return text.Substring(0, text.Length - 1);
+        }
+
+        public static string DeleteLastCharacterClassic(string text)
+        {
+            return text.Substring(0, text.Length - 1);
+        }
+
+        public static string AddQuatations(this string textToQuote)
+        {
+            return @$"""{textToQuote}""";
+        }
+    }
+}
