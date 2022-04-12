9b3d6ce6092c81ec9b06697c71bad3917a6bd2c3
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-04-12 18:40:17 +0200

    added code for first part of lesson

--- G2/Class 1/CSharpAdvanced-G2-L1-AbstractClasses/CSharpAdvanced-G2-L1-Interfaces/Program.cs
+++ G2/Class 1/CSharpAdvanced-G2-L1-AbstractClasses/CSharpAdvanced-G2-L1-Interfaces/Program.cs
@@ -1,14 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using CSharpAdvanced_G2_L1_Interfaces.Entities;
 using CSharpAdvanced_G2_L1_Interfaces.Interfaces;
+using CSharpAdvanced_G2_L1_Interfaces.Services;
 
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 
-Employee employee = new Employee("Mihajlo", 32);
+//Employee employee = new Employee("Mihajlo", 32);
 
-employee.DoWork();
+//employee.DoWork();
 
-IWork employeeTwo = new Employee("Mihajlo", 32);
+//IWork employeeTwo = new Employee("Mihajlo", 32);
 
-employeeTwo.DoWork();
+//employeeTwo.DoWork();
+
+User user = new Student("Dragan", "C# Advanced");
+
+IUserService userService = new StudentService();
+
+userService.LogIn(user);
+userService.PrintInformation(user);
+
+userService = new TeacherService();
+
+userService.LogIn(user);
+userService.PrintInformation(user);
---
+++ G2/Class 2/CSharpAdvanced-G2-L2-StaticClassesAndPolymorphism/Program.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpAdvanced_G2_L2_StaticClassesAndPolymorphism.Helpers;
+
+Console.WriteLine("Enter 1 for Student Login and 0 For Teacher Login ");
+
+int.TryParse(Console.ReadLine(), out int input);
+
+LogIn(input);
+
+
+
+
+
+
+
+
+
+
+
+
+
+void LogIn(int input)
+{
+    if (input == MenuOptions.TEACHER_LOGIN)
+    {
+        Console.WriteLine("Welcome teacher");
+    }
+    else if (input == MenuOptions.STUDENT_LOGIN)
+    {
+        Console.WriteLine("Welcome stduent");
+    }
+}
