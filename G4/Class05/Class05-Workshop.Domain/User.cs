a252212756200cd261b424344703ae30c2f5a63e
Vladimir Simonovski <vladimir.simonovski@wsa.com>
2022-04-15 17:12:37 +0200

    Workshop skeleton source code

---
+++ G4/Class05/Class05-Workshop.Domain/User.cs
@@ -0,0 +1,55 @@
+﻿namespace Class05_Workshop.Domain
+{
+    /// <summary>
+    /// <see cref="User"/> entity.
+    /// </summary>
+    public class User : BaseEntity
+    {
+        /// <summary>
+        /// Static field to keep track on what was the last assigned
+        /// Id for <see cref="User"/>, so we are able to calculate the
+        /// next one when creating a new instance (see <see cref="BaseEntity"/>
+        /// constructor in combination with <see cref="BaseEntity.GetNextEntityId"/>
+        /// on how this works).
+        /// </summary>
+        private static int s_lastEntityId = 0;
+        
+        public string UserName { get; }
+        public string Password { get; }
+        public Role Role { get; }
+
+        /// <summary>
+        /// Creates a new <see cref="User"/> instance.
+        /// Invokes <see cref="BaseEntity"/> default constructor.
+        /// </summary>
+        /// <param name="userName">User name of the user.</param>
+        /// <param name="password">Password of the user.</param>
+        /// <param name="role">Role of the created user.</param>
+        public User(string userName, string password, Role role)
+        {
+            UserName = userName;
+            Password = password;
+            Role = role;
+        }
+
+        protected override int GetNextEntityId()
+        {
+            return ++s_lastEntityId;
+        }
+
+        public override void Print()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// Enum that models user roles.
+    /// </summary>
+    public enum Role
+    {
+        Administrator,
+        Manager,
+        Maintenance
+    }
+}
---
+++ G4/Class05/README.md
@@ -0,0 +1,5 @@
+## Class05 - Workshop - Simple Taxi Application
+
+A proposed skeleton for the application that we started building during our first workshop class for this course. Already has most of the implementation in place, and tries to use all of the topics that we went through so far (abstract classes, interfaces, static classes/members, polymorphism, generics and extension methods). Some methods needs to be implemented - try to search for methods that throw `NotImplementedException`. Implementation might not be perfect though, but should give you a general idea on how to structure things - if you find anything let me know and I will correct it.
+
+Try to think of some edge cases that are not covered (e.g. whitespaces when entering userName/password) and let's discuss during next class.
