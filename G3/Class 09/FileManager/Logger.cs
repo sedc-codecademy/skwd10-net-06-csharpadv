45e4951cee685bfa80607d73c8f8a4b54a5ba98e
Wekoslav Stefanovski <swekster@gmail.com>
2022-05-04 21:12:48 +0200

    Working with files

---
+++ G3/Class 09/FileManager/Logger.cs
@@ -0,0 +1,11 @@
+﻿namespace FileManager
+{
+    internal class Logger
+    {
+        internal static void Log(int first, int second, Operation op, int result)
+        {
+            var message = $"{DateTime.Now.ToLongTimeString()}: {first} {op} {second} = {result}";
+            File.AppendAllLines("log.txt", new string[] { message });
+        }
+    }
+}
