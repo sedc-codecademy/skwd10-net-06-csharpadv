2cb972983e86486b6c6994b1b2c7e554d5204433
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-05 18:55:22 +0200

    added code for disposable

---
+++ G2/Class 9/CSharpAdvanced-G2-L10-Disposables/CustomReader.cs
@@ -0,0 +1,40 @@
+﻿namespace CSharpAdvanced_G2_L10_Disposables
+{
+    public class CustomReader : IDisposable
+    {
+
+        private readonly string _fileName;
+        private readonly FileStream _stream;
+        private bool _disposed = false;
+
+        public CustomReader(string fileName)
+        {
+            _fileName = fileName;  
+            _stream = File.OpenRead(_fileName);
+        }
+
+        public string GetFileContent()
+        {
+            return _stream.ToString();
+        }
+
+        private void Dispose(bool disposing)
+        {
+            if (!_disposed)
+            {
+                if (disposing)
+                {
+                    _stream.Close();
+                    _stream.Dispose();
+                }
+
+                _disposed = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+    }
+}
