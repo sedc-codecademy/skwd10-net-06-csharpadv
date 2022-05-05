31f956d98c1b3b8e1a6c51c64a9aa9710dd77001
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-05 20:23:14 +0200

    added code for destructors

---
+++ G2/Class 9/CSharpAdvanced-G2-L10-Disposables/Car.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpAdvanced_G2_L10_Disposables
+{
+    public class Car
+    {
+        public Driver[] Drivers { get; set; }
+
+        public Car(Driver[] drivers)
+        {
+            Drivers = drivers;
+        }
+
+
+    }
+}
