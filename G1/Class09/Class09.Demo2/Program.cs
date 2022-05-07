0738ec2b1085d2a343e5b2dfe879b4d4f177e0cb
Risto Pavlov <ristopavlov@gmail.com>
2022-05-07 08:19:12 +0200

    - Class 9 initial code

---
+++ G1/Class09/Class09.Demo2/Program.cs
@@ -0,0 +1,13 @@
+﻿#region Initial data
+//prepare paths for folder and file
+#endregion
+
+#region File manipulation
+//writing with StreamWriter
+
+// Writing without rewriting the document with StreamWriter
+
+// Reading with StreamReader
+
+//reading all text with while
+#endregion
