4c4846ef3f32c37cf58daf9e7d103a1b208460bb
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-07 15:29:26 +0200

    added code for file database

---
+++ G2/Class 10/CSharpAdvanced-G2-L10-FileDb/Database/Database.cs
@@ -0,0 +1,72 @@
+﻿using CSharpAdvanced_G2_L10_FileDb.Entities;
+using Newtonsoft.Json;
+
+namespace CSharpAdvanced_G2_L10_FileDb.Database
+{
+    public abstract class Database<T> where T : BaseEntity
+    {
+        private readonly List<T> _entities;
+        private readonly string _fileLocation;
+        private int _currentId = 0;
+
+        public Database()
+        {
+            _fileLocation =  $@"{Directory.GetCurrentDirectory()}\{typeof(T).Name}.json";
+
+            if (!File.Exists(_fileLocation))
+            {
+                var stream = File.Create(_fileLocation);
+                stream.Close();
+            }
+
+            _entities = ReadFromFile();
+            _currentId = _entities.OrderBy(x => x.Id).LastOrDefault()?.Id ?? 0;
+        }
+
+        public List<T> GetAll()
+        {
+            return _entities;
+        }
+
+        public void Insert(T item)
+        {
+            AutoIncrementId(item);
+
+            _entities.Add(item);
+
+            WriteToFile();
+        }
+
+        public void Update(T item)
+        {
+            WriteToFile();
+        }
+
+        private void AutoIncrementId(T item)
+        {
+            item.Id = ++_currentId;
+        }
+        
+        private void WriteToFile()
+        {
+            using (StreamWriter sw = new StreamWriter(_fileLocation))
+            {
+                string json = JsonConvert.SerializeObject(_entities);
+                sw.Write(json);
+                sw.Flush();
+            }
+        }
+
+        private List<T> ReadFromFile()
+        {
+            List<T> entities = new List<T>();
+            using (StreamReader sr = new StreamReader(_fileLocation))
+            {
+                string json = sr.ReadToEnd();
+                entities = JsonConvert.DeserializeObject<List<T>>(json);
+            }
+
+            return entities ?? new List<T>();
+        }
+    }
+}
---
+++ G2/Class 10/CSharpAdvanced-G2-L10-FileDb/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CSharpAdvanced_G2_L10_FileDb.Database;
+using CSharpAdvanced_G2_L10_FileDb.Entities;
+
+PersonDatabase personDb = new PersonDatabase();
+
+//Person person = new Person("Mihajlo", "Dimovski", 25, new Car(1, "KIA", "Ceed"));
+//Person person2 = new Person("Mihajlo", "Dimovski", 25, new Car(1, "KIA", "Ceed"));
+//Person person3 = new Person("Mihajlo", "Dimovski", 25, new Car(1, "KIA", "Ceed"));
+//Person person4 = new Person("Mihajlo", "Dimovski", 25, new Car(1, "KIA", "Ceed"));
+
+//personDb.Insert(person);
+//personDb.Insert(person2);
+//personDb.Insert(person3);
+//personDb.Insert(person4);
+
+while (true)
+{
+    Console.WriteLine("Current people in database");
+    Console.WriteLine("--------------------------------");
+    personDb.GetAll().ForEach(Console.WriteLine);
+    Console.WriteLine("--------------------------------");
+
+    Console.WriteLine("Enter person first name");
+    string firstName = Console.ReadLine();
+
+    Console.WriteLine("Enter person last name");
+    string lastName = Console.ReadLine();
+
+    Console.WriteLine("Enter person age");
+    string ageNonParsed = Console.ReadLine();
+    int age = int.Parse(ageNonParsed);
+
+    Person person = new Person(firstName, lastName, age, new Car(1, "KIA", "Ceed"));
+    personDb.Insert(person);
+}
