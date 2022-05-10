0097848bfbd29efc1770a146bdd951b7b35095e1
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-10 20:34:45 +0200

    last part of lesson

--- G2/Class 11/CSharpAdvanced-G2-L12-AsyncCSharp/Program.cs
+++ G2/Class 11/CSharpAdvanced-G2-L12-AsyncCSharp/Program.cs
@@ -2,54 +2,58 @@
 
 using System.Diagnostics;
 
-Stopwatch stopwatch = Stopwatch.StartNew();
+//Stopwatch stopwatch = Stopwatch.StartNew();
 
-Task<int> task = new Task<int>(() =>
-{
-    Thread.Sleep(10000);
-
-    return 1;
-});
-
-Task taskTwo = new Task(() =>
-{
-    Random rnd = new Random();
-    //throw new Exception("Test message"); This exception will get swallowed if we do not wait the task
-    Thread.Sleep(rnd.Next(10000, 11000));
-});
-
-task.Start();
-try
-{
-
-    taskTwo.Start();
-    taskTwo.Wait();
-}
-catch (Exception ex)
-{
-    Console.WriteLine(ex.Message);
-}
-
-
-//while (!task.IsCompleted)
+//Task<int> task = new Task<int>(() =>
 //{
-//    Console.WriteLine("Task is still running");
+//    Thread.Sleep(10000);
+
+//    return 1;
+//});
+
+//Task taskTwo = new Task(() =>
+//{
+//    Random rnd = new Random();
+//    //throw new Exception("Test message"); This exception will get swallowed if we do not wait the task
+//    Thread.Sleep(rnd.Next(10000, 11000));
+//});
+
+//task.Start();
+//try
+//{
+
+//    taskTwo.Start();
+//    taskTwo.Wait();
 //}
-Console.WriteLine("Waiting for task");
-task.Wait();
+//catch (Exception ex)
+//{
+//    Console.WriteLine(ex.Message);
+//}
 
-stopwatch.Stop();
-Console.WriteLine($"Task Finished. Result is {task.Result}. Total execution time {stopwatch.ElapsedMilliseconds} ms; Second task is completed {taskTwo.IsCompleted}. Is Second task failed {taskTwo.IsFaulted}");
 
-Task methodTask = DoWork();
-methodTask.Start();
-methodTask.Wait();
+////while (!task.IsCompleted)
+////{
+////    Console.WriteLine("Task is still running");
+////}
+//Console.WriteLine("Waiting for task");
+//task.Wait();
 
-Task DoWork()
+//stopwatch.Stop();
+//Console.WriteLine($"Task Finished. Result is {task.Result}. Total execution time {stopwatch.ElapsedMilliseconds} ms; Second task is completed {taskTwo.IsCompleted}. Is Second task failed {taskTwo.IsFaulted}");
+
+Console.WriteLine("Start waiting for async method");
+
+string result = await DoWorkAsync();
+
+Console.WriteLine(result);
+
+async Task<string> DoWorkAsync()
 {
-    return new Task(() =>
+   return await Task.Run(() =>
     {
         Thread.Sleep(2000);
         Console.WriteLine("Executing code returned from a method");
+
+        return "Result";
     });
 }
