a01bd51d79813602d8b8bdfe97419751d1738dd6
Mihajlo Dimovski <mihajlo.d96@hotmail.com>
2022-05-10 19:29:40 +0200

    added second part of code

---
+++ G2/Class 11/CSharpAdvanced-G2-L12-AsyncCSharp/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Stopwatch stopwatch = Stopwatch.StartNew();
+
+Task<int> task = new Task<int>(() =>
+{
+    Thread.Sleep(10000);
+
+    return 1;
+});
+
+Task taskTwo = new Task(() =>
+{
+    Random rnd = new Random();
+    //throw new Exception("Test message"); This exception will get swallowed if we do not wait the task
+    Thread.Sleep(rnd.Next(10000, 11000));
+});
+
+task.Start();
+try
+{
+
+    taskTwo.Start();
+    taskTwo.Wait();
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+
+//while (!task.IsCompleted)
+//{
+//    Console.WriteLine("Task is still running");
+//}
+Console.WriteLine("Waiting for task");
+task.Wait();
+
+stopwatch.Stop();
+Console.WriteLine($"Task Finished. Result is {task.Result}. Total execution time {stopwatch.ElapsedMilliseconds} ms; Second task is completed {taskTwo.IsCompleted}. Is Second task failed {taskTwo.IsFaulted}");
+
+Task methodTask = DoWork();
+methodTask.Start();
+methodTask.Wait();
+
+Task DoWork()
+{
+    return new Task(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Executing code returned from a method");
+    });
+}
