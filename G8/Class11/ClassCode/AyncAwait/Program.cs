ffa54db553acd06c694e53fe93d7a238afd7a6e0
petreArsovski <email@example.comarso.pet@outlook.com>
2022-05-12 20:19:19 +0200

    Class 11

---
+++ G8/Class11/ClassCode/AyncAwait/Program.cs
@@ -0,0 +1,53 @@
+﻿//SendMessages();
+
+//SendMessagesAsync("hi");
+//Console.WriteLine("Test, testing...");
+//WriteMessages();
+
+OurMainMethod();
+
+Console.ReadLine();
+
+async Task OurMainMethod()
+{
+    await SendMessagesAsync("hi");
+    WriteMessages();
+}
+
+// synchronous
+void SendMessages()
+{
+    Console.WriteLine("Hello students");
+    Thread.Sleep(5000);
+    Console.WriteLine("Goobye students");
+}
+
+//asynchronous
+async Task SendMessagesAsync(string message)
+{
+    Console.WriteLine("Start");
+    await Task.Run(() => // for making sync with async add await here
+    {
+        Thread.Sleep(3000);
+        Console.WriteLine($"Message {message} sent!");
+    });
+    Console.WriteLine("Stop");
+    //Task.Run(() =>
+    //{
+    //    Thread.Sleep(2000);
+    //    Console.WriteLine("something, some string 222");
+    //});
+    //Task.Run(() =>
+    //{
+    //    Thread.Sleep(1000);
+    //    Console.WriteLine("something, some string");
+    //});
+}
+
+//synchronous
+static void WriteMessages()
+{
+    Console.ForegroundColor = ConsoleColor.Green;
+    Console.WriteLine("This is message outside of the logic");
+    Console.ResetColor();
+}
---
+++ G8/Class11/ClassCode/Threads/Program.cs
@@ -0,0 +1,42 @@
+﻿#region Methods
+// Synchronous
+void SendMessage()
+{
+    Console.WriteLine("Getting Ready...");
+    Thread.Sleep(2000);
+    Console.WriteLine("First message arrived!");
+    Thread.Sleep(2000);
+    Console.WriteLine("Second message arrived!");
+    Thread.Sleep(2000);
+    Console.WriteLine("Third message arrived!");
+    Console.WriteLine("All messages are recaived");
+    Console.ReadLine();
+}
+// Asynchtonous
+void SendMessagesWithThreads()
+{
+    Console.WriteLine("Getting Ready...");
+    Thread myThread = new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("First message arrived!");
+    });
+    myThread.Start();  
+    new Thread(() =>
+    {
+        Thread.Sleep(2000);
+        Console.WriteLine("Second message arrived!");
+    }).Start();
+    new Thread(() =>
+    {
+        Thread.Sleep(1000);
+        Console.WriteLine("Third message arrived!");
+    }).Start();
+    Console.WriteLine("All messages are recaived");
+    Console.ReadLine();
+}
+
+#endregion
+
+//SendMessage();
+SendMessagesWithThreads();
