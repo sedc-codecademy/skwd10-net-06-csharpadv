42c85c535d5a8e7cd4f0a3c7753d5a23648a50c0
Wekoslav Stefanovski <swekster@gmail.com>
2022-05-16 20:34:44 +0200

    serialize and the other one - Class 12, Group 3

---
+++ G3/Playground/Playground/BuildingStrings/Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BuildingStrings
+{
+    internal record Person (string FirstName, string LastName, string City)
+    {
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} from {City}";
+        }
+    }
+}
