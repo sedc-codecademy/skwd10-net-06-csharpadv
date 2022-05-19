0405bdae7383a22ba54bdd01a77ee65d4245655e
petreArsovski <email@example.comarso.pet@outlook.com>
2022-05-19 22:23:14 +0200

    Class 13

---
+++ G8/Class13/Class13Code/GoodPractices/GoodPractices/Loops.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoodPractices.GoodPractices
+{
+    public class Loops
+    {
+        List<string> strings = new List<string>(){"Gorjan", "Gjporgji", "Martina", "Jovana", "Petre"};
+
+        public void LoopsExamples()
+        {
+            // Bad example
+            // Print all names that are the same lenth of the list
+            for(int counter = 0; counter < strings.Count; counter++)
+            {
+                if(strings[counter].Length == strings.Count)
+                {
+                    Console.WriteLine(strings[counter]);
+                }
+            }
+
+            // Good example
+            int listLength = strings.Count;
+            for(int counter = 0; counter < listLength; counter++)
+            {
+                if (strings[counter].Length == strings.Count)
+                {
+                    Console.WriteLine(strings[counter]);
+                }
+            }
+        }
+    }
+}
