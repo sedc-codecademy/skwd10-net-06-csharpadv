ce8325b46e6b938e7b16a357839e0cbc025e2bdf
Risto Pavlov <ristopavlov@gmail.com>
2022-05-21 22:10:18 +0200

    - class 14 code

---
+++ G1/Class14/Class14.Demo1/Practices/IfElse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class14.Demo1.Practices
+{
+    internal class IfElse
+    {
+        private bool _check = false;
+
+        //bad example
+        public void TestCheck()
+        {
+            if (_check == true)
+            {
+                Console.WriteLine("It is correct");
+            }
+        }
+
+
+        //good example
+        public void TestCheckGood()
+        {
+            //it is already bool no need to check it
+            if (_check)
+            {
+                Console.WriteLine("It is correct");
+            }
+        }
+
+        public void IsElseExample(int num1, int num2)
+        {
+            //bad example
+            //check if 2 numbers are the same
+            if(num1 <= 100 && num2 <=100)
+            {
+                if (num1 % 2 == 0 && num2 %2 == 0)
+                {
+                    if(num1 == num2)
+                    {
+                        Console.WriteLine("They are the same!");
+                    }
+                }
+            }
+
+            //good example
+            if(num1 > 100 || num2 > 100) return;
+            if(num1 % 2 != 0 || num2 % 2 != 0) return;
+            if (num1 != num2) return;
+
+            Console.WriteLine("They are the same!");
+        }
+    }
+}
---
+++ G1/Class14/Class14.Demo1/Practices/Methods.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class14.Demo1.Practices
+{
+    //Bad Example
+    public class BadService
+    {
+        public List<int> numbers = new List<int>();
+
+        public void GetStats()
+        {
+            Console.WriteLine("Welcome to the app!");
+            Console.WriteLine("Enter 5 numbers:");
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Enter number:");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+            Console.Write("You entered: ");
+            foreach (int num in numbers)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Stats:");
+            int even = numbers.Where(x => x % 2 == 0).Count();
+            Console.WriteLine($"Even numbers: {even}");
+
+            int odd = numbers.Count - even;
+            Console.WriteLine($"Odd numbers: {odd}");
+
+            int positive = numbers.Where(x => x >= 0).Count();
+            Console.WriteLine($"Positive numbers: {positive}");
+
+            int negative = numbers.Count - positive;
+            Console.WriteLine($"Negative numbers: {negative}");
+
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {sum}");
+        }
+    }
+
+    //Good example
+    public class NumberService
+    {
+        public List<int> InputNumbers(int number)
+        {
+            List<int> numbers = new List<int>();
+            Console.WriteLine("Welcome to the app!");
+            Console.WriteLine($"Enter {number} numbers:");
+            for (int i = 0; i < number; i++)
+            {
+                Console.Write("Enter number:");
+                numbers.Add(int.Parse(Console.ReadLine()));
+            }
+
+            return numbers;
+        }
+
+        public void PrintStats(List<int> numbers)
+        {
+            Console.WriteLine();
+            Console.WriteLine("Stats:");
+            int even = numbers.Where(x => x % 2 == 0).Count();
+            Console.WriteLine($"Even numbers: {even}");
+
+            int odd = numbers.Count - even;
+            Console.WriteLine($"Odd numbers: {odd}");
+
+            int positive = numbers.Where(x => x >= 0).Count();
+            Console.WriteLine($"Positive numbers: {positive}");
+
+            int negative = numbers.Count - positive;
+            Console.WriteLine($"Negative numbers: {negative}");
+
+            int sum = numbers.Sum();
+            Console.WriteLine($"Sum of numbers: {sum}");
+        }
+    }
+
+    public static class HelperService<T>
+    {
+        public static void PrintList(List<T> list)
+        {
+            foreach(T item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void PrintListInOneLine(List<T> list)
+        {
+            foreach (T item in list)
+            {
+                Console.Write(item + " ");
+            }
+        }
+    }
+
+    public class AppService
+    {
+        private NumberService _numberService;
+        private List<int> _numbers;
+
+        public AppService()
+        {
+            _numberService = new NumberService();
+        }
+
+        public void AppInit()
+        {
+            //input numbers
+            _numbers = _numberService.InputNumbers(5);
+
+            //print data
+            Console.WriteLine("You have entered: ");
+            HelperService<int>.PrintListInOneLine(_numbers);
+
+            //print stats
+            _numberService.PrintStats(_numbers);
+
+        }
+    }
+}
